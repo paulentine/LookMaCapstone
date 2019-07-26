b154e4f73bd5837be96001489e67ecb0d3c43534
Pauline Sauget <paulinevsauget@gmail.com>
2019-07-26 16:28:20 -0700

    Nested step fields are dynamically-generated and submits data in the correct format. Good working state.

--- app/src/main/java/com/paulentine/android/capstone/NewRecipeActivity.java
+++ app/src/main/java/com/paulentine/android/capstone/NewRecipeActivity.java
@@ -1,12 +1,15 @@
 package com.paulentine.android.capstone;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -26,21 +29,30 @@ public class NewRecipeActivity extends AppCompatActivity {
     private EditText editTextTitle;
     private EditText editTextReadyInMinutes;
     private EditText editTextServings;
-    private EditText editTextSteps;
+    private int stepsCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_recipe);
 
-//        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close_white_24px);
-//        setTitle("Add Recipe");
-
         editTextId = findViewById(R.id.edit_text_id);
         editTextTitle = findViewById(R.id.edit_text_title);
         editTextReadyInMinutes = findViewById(R.id.edit_text_ready_in_minutes);
         editTextServings = findViewById(R.id.edit_text_servings);
-        editTextSteps = findViewById(R.id.edit_text_steps);
+
+        final LinearLayout stepsLayout = findViewById(R.id.steps_linear_layout);
+        final Button mNewStepButton = findViewById(R.id.recipe_button_new_step);
+        mNewStepButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText editText = new EditText(stepsLayout.getContext());
+                stepsCount++;
+                Log.d("stepsCount", Integer.toString(stepsCount));
+//                editText.setText("TextView " + String.valueOf(stepsCount));
+                stepsLayout.addView(editText);
+            }
+        });
 
         final Button mSaveButton = findViewById(R.id.recipe_button_save);
         mSaveButton.setOnClickListener(new View.OnClickListener() {
@@ -56,6 +68,15 @@ public class NewRecipeActivity extends AppCompatActivity {
         String title = editTextTitle.getText().toString();
         int readyInMinutes = Integer.parseInt(editTextReadyInMinutes.getText().toString());
         int servings = Integer.parseInt(editTextServings.getText().toString());
+        List<Step> steps = new ArrayList<Step>();
+
+        final LinearLayout stepsLayout = findViewById(R.id.steps_linear_layout);
+        int count = stepsLayout.getChildCount();
+        for (int stepNum = 1; stepNum < stepsCount; stepNum++) {
+            EditText stepField = (EditText)stepsLayout.getChildAt(stepNum);
+            String stepInstruction = stepField.getText().toString();
+            steps.add(new Step(stepNum, stepInstruction));
+        }
 
         if (title.trim().isEmpty()) {
             Toast.makeText(this, "Please insert a title", Toast.LENGTH_SHORT).show();
@@ -65,51 +86,9 @@ public class NewRecipeActivity extends AppCompatActivity {
         CollectionReference recipesRef = FirebaseFirestore.getInstance()
                 .collection("testRecipes");
 
-        // Fake list of steps
-        List<Step> fakeSteps = new ArrayList<Step>();
-        fakeSteps.add(new Step(1, "Turn on oven"));
-        fakeSteps.add(new Step(2, "Turn off oven"));
-        fakeSteps.add(new Step(3, "Drink water"));
-        recipesRef.add(new Recipe(id, title, readyInMinutes, servings, fakeSteps));
-//
-//        // Got this from some tutorial, obv doesn't work cos data structure doesn't match
-//        String stepInput = editTextSteps.getText().toString();
-//        String[] stepArray = stepInput.split("\\s*,\\s*");
-//        List<String> steps = Arrays.asList(stepArray);
-//        recipesRef.add(new Recipe(id, title, readyInMinutes, servings, steps));
-
-//        // TODO: Match data structure w/ form so we can send real steps
-//        // For each step from our input, we want to instantiate a new Step, with number & instruction
-//        recipesRef.add(new Recipe(id, title, readyInMinutes, servings, steps));
+        recipesRef.add(new Recipe(id, title, readyInMinutes, servings, steps));
 
         Toast.makeText(this, "Recipe added", Toast.LENGTH_SHORT).show();
         finish();
     }
 }
-
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        MenuInflater menuInflater = getMenuInflater();
-//        menuInflater.inflate(R.menu.new_recipe_menu, menu);
-//        return super.onCreateOptionsMenu(menu);
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        switch (item.getItemId()) {
-//            case R.id.save_recipe:
-//                saveRecipe();
-//                return true;
-//            default:
-//                return super.onOptionsItemSelected(item);
-//        }
-//    }
-//    public boolean onSaveButtonClicked(Button button) {
-//        switch (button.getItemId()) {
-//            case R.id.save_recipe:
-//                saveRecipe();
-//                return true;
-//            default:
-//                return super.onSaveButtonClicked(button);
-//        }
-//    }
--- app/src/main/java/com/paulentine/android/capstone/model/Recipe.java
+++ app/src/main/java/com/paulentine/android/capstone/model/Recipe.java
@@ -1,19 +1,4 @@
-/**
- * Copyright 2017 Google Inc. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
- package com.paulentine.android.capstone.model;
+package com.paulentine.android.capstone.model;
 
 import com.google.firebase.firestore.IgnoreExtraProperties;
 
@@ -33,6 +18,7 @@ public class Recipe {
     private int readyInMinutes;
     private int servings;
     private List<Step> steps;
+//    private int currentStep;
 
 //    private String image;
 
@@ -68,6 +54,8 @@ public class Recipe {
         this.readyInMinutes = readyInMinutes;
         this.servings = servings;
         this.steps = steps;
+//        this.currentStep = 0;
+
 //        this.image = image;
     }
 
@@ -110,6 +98,11 @@ public class Recipe {
     public void setSteps(List<Step> steps) {
         this.steps = steps;
     }
+
+//    public int getCurrentStep() {
+//        return currentStep;
+//    }
+
 
 //    public String getImage() {
 //        return image;
